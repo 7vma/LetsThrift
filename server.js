d49dfcca204fbe567af72fe8b5fb8a8abe982c3f
7vma <robles23david@gmail.com>
2022-10-07 13:51:25 -0700

    first commit

---
+++ server.js
@@ -0,0 +1,29 @@
+///////////////////////////////
+// DEPENDENCIES
+////////////////////////////////
+
+// initialize .env variables
+require("dotenv").config();
+
+const mongoose = require('mongoose');
+// pull PORT from .env, give default value of 4000 and establish DB Connection
+const { PORT, MONGODB_URI } = process.env || 4000;
+
+// import express
+const express = require("express");
+
+// create application object
+const app = express();
+	
+	///////////////////////////////
+	// ROUTES
+	////////////////////////////////
+	// create a test route
+	app.get("/", (req, res) => {
+    res.send("hello world");
+	});
+
+///////////////////////////////
+// LISTENER
+////////////////////////////////
+app.listen(PORT, () => console.log(`listening on PORT ${PORT}`));
